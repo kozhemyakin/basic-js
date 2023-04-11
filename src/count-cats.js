a15c3467b173a4145f44a9aa11844858416bda12
kozhemyakin <pctdoc@gmail.com>
2023-04-11 20:46:20 +0300

    fix: solve count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function countCats(params) {
+  const flat = params.flat();
+  const res = flat.filter(ear => ear === '^^');
+
+  return res.length;
 }
 
 module.exports = {
