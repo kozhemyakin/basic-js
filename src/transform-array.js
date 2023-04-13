e977030acf379ef6fe990cf19d1bc8bab7e322f8
kozhemyakin <pctdoc@gmail.com>
2023-04-13 23:06:28 +0300

    fix: transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,67 +14,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function transform(arr) {
-  const emptyArr = [...arr];
-  console.debug(emptyArr)
-  if (!Array.isArray(arr)) {
-    // throw new Error("'\'arr\' parameter must be an instance of the Array!'")
-    return false
-  } else if (arr.length == 0) {
+  let res = [];
 
-    return []
-  } else {
-    emptyArr.forEach((el) => {
-      if (typeof el !== 'number') {
-        delete el
-      }
-    })
-    // console.debug('emptyArr', emptyArr)
-    delete emptyArr[0]
-    delete emptyArr[1]
-    
-    // emptyArr.forEach((el, index) => {
-    //   if (el === '--double-next') {
-    //     emptyArr[index] = emptyArr[index + 1];
-    //   }
-    //   if (el === '--double-prev' && index == 0) {
-    //     delete emptyArr[0];
-    //   }
-    //   if (el === '--double-prev' && index != 0) {
-    //     emptyArr[index] = emptyArr[index - 1];
-    //   }
-    //   if (el === '--discard-next') {
-    //     delete emptyArr[index + 1];
-    //     delete emptyArr[index];
-    //   }
-    //   if (el === '--discard-prev' && index == 0) {
-    //     delete emptyArr[0];
-    //   }
-    //   if (el === '--discard-prev' && index != 0) {
-
-    //     delete emptyArr[index - 1];
-    //     delete emptyArr[index - 1];
-    //   }
-    // })
-
-    // console.debug(emptyArr)
-    // // const doubleNext = emptyArr.findIndex(el => el === '--double-next');
-    // const doublePrev = emptyArr.findIndex(el => el === '--double-prev');
-    // const discardNext = emptyArr.findIndex(el => el === '--discard-next');
-    // const discardPrev = emptyArr.findIndex(el => el === '--discard-prev');
-
-
-    // console.debug(doubleNext, doublePrev, discardNext, discardPrev)
-    // console.debug('doubleNext', doubleNext)
-    // if (doubleNext >= 0) emptyArr[doubleNext] = emptyArr[doubleNext + 1];
-    // if (doublePrev >= 0) emptyArr[doublePrev] = emptyArr[doublePrev - 1];
-    // if (discardNext >= 0) emptyArr = emptyArr.toSpliced(discardNext, 1);
-    // if (discardPrev >= 0) emptyArr = emptyArr.toSpliced(discardPrev, 1);
-
-    // toSpliced(1, 1, "Feb", "Mar");
-
-
-    return emptyArr
+  if(!Array.isArray(arr)){
+    throw new Error(`'arr' parameter must be an instance of the Array!`);
   }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next') { 
+
+      i++;
+    } else if (arr[i] === '--discard-prev') {
+      if (i > 0 && res[res.length - 1] === arr[i - 1]) {
+
+        res.pop();
+      }
+    } else if (arr[i] === '--double-next') {
+      if (i < arr.length - 1) {
+
+        res.push(arr[i + 1]);
+      }
+    } else if (arr[i] === '--double-prev') {
+      if(i > 0 && res[res.length - 1] === arr[i - 1]) {
+
+        res.push(arr[i - 1]);
+      }
+    } else if (typeof arr[i] !== 'undefined') {
+      res.push(arr[i]);
+    }
+  }
+  
+  return res;
 }
 
 module.exports = {
